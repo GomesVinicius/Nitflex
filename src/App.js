2f369b5ec3087cc350b058dd2274fa0510ef1fb9
Vinícius Gomes <vinigc19@gmail.com>
2020-07-28 20:44:33 -0300

    Mostando informações na arquitetura correta

--- src/App.js
+++ src/App.js
@@ -1,49 +1,10 @@
 import React from 'react';
-
-import Menu from './components/Menu'
-import BannerMain from './components/BannerMain'
-import Carousel from './components/Carousel'
-import Footer from './components/Footer'
-
-import dadosInicias from './data/dados_iniciais.json'
+import Home from './pages/home';
 
 function App() {
   return (
     <div style={{ background: "#141414" }}>
-      <Menu />
-
-      <BannerMain 
-        videoTitle={dadosInicias.categorias[0].videos[0].titulo}
-        url={dadosInicias.categorias[0].videos[0].url}
-        videoDescription={'O que é Front End?'}
-      />
-
-      <Carousel
-        ignoreFirstVideo
-        category={dadosInicias.categorias[0]}
-      />
-
-      <Carousel
-        ignoreFirstVideo
-        category={dadosInicias.categorias[1]}
-      />
-
-      <Carousel
-        ignoreFirstVideo
-        category={dadosInicias.categorias[2]}
-      />
-      
-      <Carousel
-        ignoreFirstVideo
-        category={dadosInicias.categorias[3]}
-      />      
-
-      <Carousel
-        ignoreFirstVideo
-        category={dadosInicias.categorias[4]}
-      />
-
-      <Footer />
+      <Home />
     </div>
   );
 }
--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,9 +1,50 @@
 import React from 'react'
+import BannerMain from '../../components/BannerMain'
+import Menu from '../../components/Menu'
+import Footer from '../../components/Footer'
+import Carousel from '../../components/Carousel'
+
+
+import dadosInicias from '../../data/dados_iniciais.json'
+
 
 function Home() {
     return(
         <div>
-            
+            <Menu />
+
+            <BannerMain 
+                videoTitle={dadosInicias.categorias[0].videos[0].titulo}
+                url={dadosInicias.categorias[0].videos[0].url}
+                videoDescription={'O que é Front End?'}
+            />
+
+            <Carousel
+                ignoreFirstVideo
+                category={dadosInicias.categorias[0]}
+            />
+
+            <Carousel
+                ignoreFirstVideo
+                category={dadosInicias.categorias[1]}
+            />
+
+            <Carousel
+                ignoreFirstVideo
+                category={dadosInicias.categorias[2]}
+            />
+
+            <Carousel
+                ignoreFirstVideo
+                category={dadosInicias.categorias[3]}
+            />      
+
+            <Carousel
+                ignoreFirstVideo
+                category={dadosInicias.categorias[4]}
+            />
+
+            <Footer />
         </div>
     )
 }
